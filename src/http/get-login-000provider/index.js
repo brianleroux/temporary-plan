d9524d972a643ca0e23f9b28bb564bfe9d83144d
Brian LeRoux <b@brian.io>
2019-01-06 20:27:18 -0800

    Update index.js

--- src/http/get-login-000provider/index.js
+++ src/http/get-login-000provider/index.js
@@ -1,8 +1,11 @@
 let tiny = require('tiny-json-http')
 
 exports.handler = async function http(req) {
+  
+  // trade the code for an access token
   let result = await tiny.post({
     url: 'https://github.com/login/oauth/access_token', 
+    headers: {Accept: 'application/json'},
     data: {
       code: req.query.code,
       client_id: process.env.GITHUB_CLIENT_ID,
@@ -10,8 +13,17 @@ exports.handler = async function http(req) {
       redirect_uri: process.env.GITHUB_REDIRECT,
     }
   })
+  
+  let access_token = result.body.access_token
+  
+  // use the access token to get the user account
+  let account = await tiny.get({
+    url: `https://api.github.com/user?access_token=${access_token}`,
+    headers: {Accept: 'application/json'},
+  })
+  
   return {
     type: 'text/html; charset=utf8',
-    body: '<pre>'+JSON.stringify(result)
+    body: '<pre>'+JSON.stringify(account, null ,2)+'</pre>'
   }
 }
