f3798f6d29f4f07b7fc876f68cf286047e6f1302
Brian LeRoux <b@brian.io>
2019-01-06 21:52:28 -0800

    Update slack.js

--- src/http/get-login-000provider/slack.js
+++ src/http/get-login-000provider/slack.js
@@ -1,5 +1,24 @@
 let tiny = require('tiny-json-http')
 
 module.exports = async function slack(req) {
-  return {}
+  
+  // trade the code for an access token
+  let result = await tiny.get({
+    url: `https://slack.com/api/oauth.access`
+    data: {
+      client_id: process.env.SLACK_CLIENT_ID,
+      client_secret: process.env.SLACK_CLIENT_SECRET,
+      code: req.query.code,
+    }
+  })
+  
+  let token = result.body.access_token
+  
+  // get the user account 
+  let account = await tiny.get({
+    url: 'https://slack.com/api/users.identity'
+    data: {token}
+  })
+  
+  return {token, ...account.body.user}
 }
