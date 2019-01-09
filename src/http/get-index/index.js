049b839a52c0d9a874cc14997e421f4319730b35
kj <kristoferjoseph@users.noreply.github.com>
2019-01-08 17:57:22 -0800

    Fixes issue with redirect_uri

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -2,7 +2,7 @@ let begin = require('@architect/functions')
 
 function render(session) { 
   let slack = `<li><a href="https://slack.com/oauth/authorize?scope=identity.basic,identity.email,identity.team,identity.avatar&client_id=${process.env.SLACK_CLIENT_ID}"><img alt="Sign in with Slack" height="40" width="172" src="https://platform.slack-edge.com/img/sign_in_with_slack.png" srcset="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x" /></a>`
-  let login = `<li><a href=https://github.com/login/oauth/authorize?redirect_url=${process.env.GITHUB_REDIRECT}&client_id=${process.env.GITHUB_CLIENT_ID}>login with github</a>`
+  let login = `<li><a href=https://github.com/login/oauth/authorize?redirect_uri=${process.env.GITHUB_REDIRECT}&client_id=${process.env.GITHUB_CLIENT_ID}>login with github</a>`
   let logout = `<a href=/logout>logout</a>`
   return `
 <!doctype html>
