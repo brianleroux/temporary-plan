ff280426c42d0474e170e478bc54cae8bb2e83e2
Brian LeRoux <b@brian.io>
2019-01-11 10:10:26 -0800

    Update index.js

--- src/http/get-t/index.js
+++ src/http/get-t/index.js
@@ -1,7 +1,7 @@
 exports.handler = async function http(req) {
   console.log(req)
   return {
-    type: 'applicatoin/json; charset=utf8',
+    type: 'application/json; charset=utf8',
     body: JSON.stringify({
     "text": "It's 80 degrees right now.",
     "attachments": [
